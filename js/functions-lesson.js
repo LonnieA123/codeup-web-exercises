05ad05a2664d6c6d3743d53081ef40c0eab0b6af
Lonnie Arellano <lonnie.j.arellano@gmail.com>
2022-11-21 16:49:52 -0600

    Added Everything

---
+++ js/functions-lesson.js
@@ -0,0 +1,22 @@
+//let username = prompt(name:'jason')
+//console.log(message);
+
+fight("billy", "bob")
+
+isEven(2)
+
+
+
+function myFunction(name){
+    return "thanks " + name + "you have ran my function"
+}
+
+function fight(person1, person2) {
+ console.log(person1 + " punched " + person2 + " in the face ")
+}
+
+
+function isEven(num){
+  return  (num % 2 == 0);
+}
+
