686372b7016f92784b4ca5bd18fa1728ac257eca
Lonnie Arellano <lonnie.j.arellano@gmail.com>
2022-12-02 10:06:44 -0600

    created a number of loops

---
+++ js/break_and_continue.js
@@ -0,0 +1,18 @@
+//
+//
+// while(true){
+//     let userNum = prompt("enter a odd number between 1 and 50")
+//     userNum = parseFloat(userNum)
+//     if(userNum % 2 === 1 && userNum <= 50 && userNum >= 1 ){
+//         for(let i = 1; i <= 50; i+= 2){
+//             if ( i === userNum){
+//                 continue
+//             } else {
+//                 console.log(i)
+//             }
+//         }
+//         break;
+//     }
+// }
+
+
---
+++ js/while.js
@@ -0,0 +1,55 @@
+
+let result6
+
+function random(min, max,){
+    return Math.floor(Math.random() * (max - min + 1 ) + max);
+}
+
+
+let conesToBuy = random(1,5)
+let conesToSell = random(50,100)
+
+
+
+
+do {
+    let conesToSell = random(50, 100);
+    let conesToBuy = random(1,5);
+
+    if (conesToBuy <= conesToSell) {
+        console.log(`${conesToBuy}cones sold`)
+        conesToSell = conesToSell - conesToBuy
+
+     } else {
+        console.log('no more cones')
+        break;
+
+     }
+
+} while (conesToSell > 0)
+
+
+
+
+
+// let numMulti = 2;
+// let counter = 0;
+//
+// while(counter < 16) {
+//     console.log(numMulti)
+//     numMulti = numMulti * 2
+//     counter ++
+// }
+
+    //
+    // for (let i = 0, num = 2; i < 16; i++) {
+    //     console.log(num);
+    //     num *= 2;
+    // }
+
+
+
+
+
+
+
